4610b0fe0a6aa467a4b2eb1a009afc5e81204ef4
ramesh <ramesh@nirvaha.com>
2018-01-25 19:32:49 -0800

    First commit

---
+++ proxy.js
@@ -0,0 +1,68 @@
+var http = require('http');
+
+http.createServer(onRequest).listen(3023);
+
+var cluster1Address = "35.184.176.29";
+var cluster2Address = "35.184.176.29";
+
+if(process.env.cluster1Address != null)
+{
+    cluster1Address = process.env.cluster1Address;
+}
+
+if(process.env.cluster2Address != null)
+{
+    cluster2Address = process.env.cluster2Address;
+}
+
+
+
+function onRequest(client_req, client_res) {
+  // console.log('serve: ' + client_req.url);
+
+
+  client_req.uniqueLogId = (new Date()).getTime();
+  // console.log(client_req.uniqueLogId);
+
+  var options1 = {
+    hostname: cluster1Address, // 'www.google.com',
+    port: 80,
+    path: client_req.url,
+    method: 'GET'
+  };
+
+  var options2 = {
+    hostname: cluster2Address, // 'www.google.com',
+    port: 80,
+    path: client_req.url,
+    method: 'GET'
+  };
+
+
+  var proxy1 = http.request(options1, function (res) {
+    res.pipe(client_res, {
+      end: true
+    });
+  });
+
+  client_req.pipe(proxy1, {
+    end: true
+  });
+
+
+
+
+  var proxy2 = http.request(options2, function (res2) {
+    /*
+    res2.pipe(client_res, {
+      end: true
+    });
+    */
+  });
+
+  client_req.pipe(proxy2, {
+    end: true
+  });
+
+
+}
