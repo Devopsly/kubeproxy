2c751d18a345a42a206a93eec8d7e91693804c85
ramesh <ramesh@nirvaha.com>
2018-02-17 11:38:53 -0800

    hopefully working code

--- proxyssl.js
+++ proxyssl.js
@@ -4,14 +4,14 @@ var fs = require('fs');
 
 var options = 
 {
-  key: fs.readFileSync('ssl/windocks.com.key'),
-  cert: fs.readFileSync('ssl/windocks.com.crt')
+  key: fs.readFileSync('/etc/ssl-glowforge/glowforge.com.crt'),
+  cert: fs.readFileSync('/etc/ssl-glowforge/glowforge.com.key')
 };
 
 
 https.createServer(options, onRequestS).listen(3024);
 
-var cluster1Address = "windocks.com" ; // "app.glowforge.com"; //  "130.211.155.236" ; // GFCORE prod LB // "35.197.31.30";
+var cluster1Address =  "app.glowforge.com"; //  "130.211.155.236" ; // GFCORE prod LB // "35.197.31.30";
 var cluster2Address = "manufacturing.glowforge.com";  // "146.148.41.230" ; // GFCORE manufacturing LB // "35.184.176.29";
 
 if(process.env.cluster1Address != null)
